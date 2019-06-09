0cbfabc712f371a01da23df14950532e881185db
Jay <kaosps2@gmail.com>
2019-06-09 23:48:17 +0800

    WIP2
    added basic functions (now displays artist and track)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 spotify-lyrics
+
+This small shell script uses Spotify-now(https://github.com/micahco/spotify-now), MusixMatch API(https://developer.musixmatch.com/) perl and curl to provide lyrics in CLI.
+
+Simply run this shell script and it will display lyrics for you  
+
+
--- main.sh
+++ main.sh
@@ -3,19 +3,37 @@
 # check if spotify is running
 ERROR="Spotify is not running! Shutting down......"
 status=`pidof spotify | wc -l`
-if [[ "$status" != 1  ]]; then
+if [ "$status" != 1  ] ; then
     echo "$ERROR"
-else
-	echo "Spotify is running."
+    exit 1
+	#echo "Spotify is running."
 fi
 
 #Should check if anything is playing while in loop
 while true; do
 	#check if anything is playing, if not then throw an error
+	#but there is always something playing in spotify so we'll skip that
 
 	#then if something is playing, use spotify-now to write things into variables
-	echo "Program"
-	break
+	sp_artist=$(./spotify-now -i %artist)
+	sp_title=$(./spotify-now -i %title)
+	echo "$sp_title - $sp_artist"
+	#before retrieving lyrics we will use perl to encode and escape variables
+	sp_title=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$sp_title")
+	sp_artist=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$sp_artist")
+	#echo "$sp_title - $sp_artist"
 
+	#use curl to retrieve tracklist from Musixmatch with specified artist and track title
+
+	#use curl to retireve lyrics from Musixmatch
+	
+
+
+	#see if in another track if not then sleep
+	while [ "$(./spotify-now -i %artist)" != "$sp_artist" ] 
+	do
+		echo "nothing changes"
+		sleep 30s
+	done
 
 done
