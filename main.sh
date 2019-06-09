2ec58b98372bdf658d9d279a1ece598dec492860
ero3322001 <50433890+ero3322001@users.noreply.github.com>
2019-06-10 00:02:53 +0800

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # check if spotify is running
+apikey_musixmatch= ''
+apiurl_musixmatch = 'http://api.musixmatch.com/ws/1.1/'
+
 ERROR="Spotify is not running! Shutting down......"
 status=`pidof spotify | wc -l`
 if [ "$status" != 1  ] ; then
@@ -24,6 +27,10 @@ while true; do
 	#echo "$sp_title - $sp_artist"
 
 	#use curl to retrieve tracklist from Musixmatch with specified artist and track title
+	match_title=$(curl --get --include "'https://musixmatchcom-	musixmatch.p.rapidapi.com/wsr/1.1/track.search?	s_artist_rating=desc&q_artist='$sp_artist'&q_track='$sp_title"\
+  	-H "'X-RapidAPI-Host:' $apiurl_musixmatch " \
+  	-H "'X-RapidAPI-Key:' $apikey_musixmatch")
+	track_id=$(curl "$match_title" | jq '.[].body.track_list | .[0].track.track_id')
 
 	#use curl to retireve lyrics from Musixmatch
 	
