760f860cb9dda4cc46ef13b1e46f408315c3cd70
Jay <kaosps2@gmail.com>
2019-06-10 02:53:03 +0800

    Done
    Final commit maybe

--- newmain.sh
+++ newmain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-apikey_musixmatch="58db4e1d7c78009f4345c8446298ea58"
+apikey_musixmatch=""
 apiurl_musixmatch='http://api.musixmatch.com/ws/1.1/'
 # check if spotify is running
 ERROR="Spotify is not running! Shutting down......"
@@ -27,26 +27,29 @@ do
 	#before retrieving lyrics we will use perl to encode and escape variables
 	sp_title=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$sp_title")
 	sp_artist=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$sp_artist")
-	#echo "$sp_title - $sp_artist"
 
 	#use curl to retrieve tracklist from Musixmatch with specified artist and track title
 	match_title=$(curl -s "${apiurl_musixmatch}/track.search?apikey=${apikey_musixmatch}&s_artist_rating=desc&q_artist=$sp_artist&q_track=$sp_title" | ./jq '.[].body.track_list | .[0].track.track_id')
 	echo "$match_title"
-	#track_id=$( "$match_title" | ./jq '.[].body.track_list | .[0].track.track_id')
 
+
+	#track_id=$( "$match_title" | ./jq '.[].body.track_list | .[0].track.track_id')
 	#use curl to retireve lyrics from Musixmatch
 	#match_lyrics=$(curl -s "${apiurl_musixmatch}/track.lyrics.get?apikey=${apikey_musixmatch}&track_id=$track_id")
 
+
 	match_lyrics=$(curl -s "${apiurl_musixmatch}/track.lyrics.get?apikey=${apikey_musixmatch}&track_id=$match_title" | ./jq '.[].body.lyrics.lyrics_body' )
-  	#-H "'X-RapidAPI-Host:' $apiurl_musixmatch " \
+  	
+	
+	
+	#-H "'X-RapidAPI-Host:' $apiurl_musixmatch " \
   	#-H "'X-RapidAPI-Key:' $apikey_musixmatch")
 	#title_lyrics=$("$match_lyrics" | ./jq '.[].body.lyrics.lyrics_body')
-	
-	echo "$title_lyrics \n"
+	clear	
+	echo "$match_lyrics \n"
 	#see if in another track if not then sleep
 	while [ "$(./spotify-now-master/spotify-now -i %artist)" != "$sp_unencode_title" ] 
 	do
-		echo "nothing changes"
 		sleep 30s
 	done
 
